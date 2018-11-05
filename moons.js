ce0ce532e15b968cf73b9d2bf089e22a13bd6ed0
stekra <24462571+stekra@users.noreply.github.com>
2018-11-05 16:58:25 +0100

    Update moons.js

--- moons.js
+++ moons.js
@@ -5,8 +5,8 @@ let moons = document.getElementsByClassName("moon");
 
 function update() {
     if (!time) {
-        for (let i = 0; i < moons.length; i++) {
-            moons[i].innerText = lookup[phase];
+        for (let moon of moons) {
+            moon.innerText = lookup[phase];
         }
         phase = (phase + 1) % lookup.length;
         time = 20;
